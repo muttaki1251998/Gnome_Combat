f6d2c07e6803d034ab06de33c83423ff83c835fe
A Muttaki Khan <36711043+muttaki1251998@users.noreply.github.com>
2018-06-09 12:40:37 +0600

    First Commit
    1. Player movement added
    2. Camera follower added

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    private float smoothing = 5.0f;
+
+    Vector3 offset;
+
+    private void Awake()
+    {
+        //Assert.IsNotNull(target);
+    }
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 cameraPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, cameraPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 50.0f;
+
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMovement();
+	}
+
+    void PlayerMovement()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.SimpleMove(move * moveSpeed);
+    }
+}
